fdddb7147700baa0d532cff677232799faa691aa
Vikash KUmar Yadav <yadav621gzp@gmail.com>
2023-06-20 12:55:52 +0530

    removed the library

--- components/Hero/hero.jsx
+++ components/Hero/hero.jsx
@@ -16,9 +16,9 @@ import { Modal, Input, Tooltip } from 'antd'
 const Hero = () => {
   return (
     <>
-      <div name="about" className=" h-screen max-md:h-full w-full pb-72 bg-gradient-to-br ">
+      <div name="about" className="hero h-screen max-md:h-full w-full pb-72 bg-gradient-to-br ">
         <div className="max-w-screen-lg  mx-auto flex flex-col-reverse items-center justify-between h-full md:flex-row">
-          <div className = " mb-32 pr-12 flex flex-col max-md:items-center justify-center ">
+          <div className = "hero bg-blue mb-32 pr-12 flex flex-col max-md:items-center justify-center ">
             <h5 className = "text-blue-400 max-md:text-1xl text-3xl font-bold flex flex-col max-md:items-center justify-center">
               <div className = "text-blue text-1xl max-md:text-2xl py-6 mb-3">Decentralised Application for </div> 
               
@@ -41,7 +41,7 @@ const Hero = () => {
             </div>
           </div>
 
-          <div className = "pl-12 ml-13 flex flex-col items-center justify-center md:hover:scale-125 duration-300">
+          <div className = "hero pl-12 ml-13 flex flex-col items-center justify-center md:hover:scale-125 duration-300">
             <Image src = {ProfilePic} height="550" width="650" className = " max-md:pt-10 " />
             <Link href='/Market'>
             <div className='pr-full'><button className="group font-semibold  text-white w-fit px-6 py-3 my-2  flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
